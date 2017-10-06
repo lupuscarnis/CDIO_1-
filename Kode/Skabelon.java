aca4f0e21e2c8d8230b80bcfbe49b8fd310bd72f
Darthlatte <savethecat@gmail.com>
2017-10-06 13:25:06 +0200

    Hello World
    Skabelon som printer hello world

---
+++ Kode/Skabelon.java
@@ -0,0 +1,14 @@
+
+public class Skabelon {
+
+	public static void main(String[] args) {
+
+        //	Dato: 01/01-2017
+        //	Navn: Kasper
+        //	Kommentar: abcdefghijklmn
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
