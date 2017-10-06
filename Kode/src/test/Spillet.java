827cca2fa0316262328b1eba80f5b17874cfff2e
Darthlatte <savethecat@gmail.com>
2017-10-06 14:16:18 +0200

    Fix
    fixi

--- Kode/src/test/Spillet.java
+++ Kode/src/test/Spillet.java
@@ -8,16 +8,12 @@ public class Spillet {
 		// Frederik var her
 		System.out.println("Hello world!");
 		
-		
-<<<<<<< HEAD
-		
-=======
+
 		Frederik var her
 	//Hej hilsen Fredrik
 		
 		//Flotfyr23
 	
->>>>>>> b3765a190cfc612450361956edf7c13a6a8df908
 	}
 	
 }
