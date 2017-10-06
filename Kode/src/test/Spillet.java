9e2ce02bccfdbdd2b427f765d4bb4e2ab17d41dc
Frederik <frederik@mybid.dk>
2017-10-06 14:13:50 +0200

    Flyttet mit navn

--- Kode/src/test/Spillet.java
+++ Kode/src/test/Spillet.java
@@ -5,10 +5,11 @@ public class Spillet {
 	public static void main(String[] args)
 	{
 		// Nicolai Barnett
+		// Frederik var her
 		System.out.println("Hello world!");
 		
 		
-		Frederik var her
+		
 	}
 	
 }
