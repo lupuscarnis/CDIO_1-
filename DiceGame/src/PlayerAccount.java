b130094ab0b945f32b25e55c5639f775fe1f2a8a
163312 <32563877+163312@users.noreply.github.com>
2017-10-29 18:31:55 +0100

    foerste commit

---
+++ DiceGame/src/PlayerAccount.java
@@ -0,0 +1,29 @@
+
+public class PlayerAccount {
+
+	private int balance;
+	
+	public PlayerAccount() {
+		
+		balance=1000;
+	}
+	
+	withdraw() {
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
